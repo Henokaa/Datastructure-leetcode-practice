9dc6150a441454c5d8848c4b8f9319dd08930947
Henok F <69524541+Henokaa@users.noreply.github.com>
2022-01-08 13:57:16 +0300

    Time: 44 ms (58.74%), Space: 14.2 MB (61.60%) - LeetHub

--- 299-bulls-and-cows/299-bulls-and-cows.py
+++ 299-bulls-and-cows/299-bulls-and-cows.py
@@ -1,17 +1,12 @@
-class Solution(object):
-    def getHint(self, secret, guess):
-      
-        bulls=set()
-        for i,j in enumerate(guess):
-            if j==secret[i]:
-                bulls.add(i)
-        counts={}
-        for i,j in enumerate(secret):
-            if i not in bulls:
-                counts[j]=counts.get(j,0)+1
-        b=0
-        for i,j in enumerate(guess):
-            if i not in bulls and counts.get(j,0):
-                b+=1
-                counts[j]-=1
-        return '%dA%dB'%(len(bulls),b)
+class Solution:
+    def getHint(self, secret: str, guess: str) -> str:
+        bulls = 0
+        bucket = [0] * 10
+        
+        for s, g in zip(secret, guess):
+            if s == g:
+                bulls += 1
+            else:
+                bucket[int(s)] += 1
+                bucket[int(g)] -= 1
+        return f'{bulls}A{len(secret) - bulls - sum(x for x in bucket if x > 0)}B'
