16dec1a342f268a7211b0e41520a989973741fde
Henok F <69524541+Henokaa@users.noreply.github.com>
2022-02-03 15:53:30 +0300

    Time: 49 ms (17.03%), Space: 13.8 MB (98.36%) - LeetHub

--- 394-decode-string/394-decode-string.py
+++ 394-decode-string/394-decode-string.py
@@ -1,22 +1,19 @@
 class Solution:
-    def __init__(self):
-        self.i = 0
-
     def decodeString(self, s: str) -> str:
-        result = ""
-        while self.i < len(s) and s[self.i] != "]":
-            if not s[self.i].isdigit():
-                result += s[self.i]
-                self.i += 1
+        res, num = '',0
+        stack = []
+        for c in s:
+            if c.isdigit():
+                num = num*10 + int(c)
+            elif c == '[':
+                stack.append(res)
+                stack.append(num)
+                res = ''
+                num = 0
+            elif c == ']':
+                snum = stack.pop()
+                sstr = stack.pop()
+                res = sstr + snum*res
             else:
-                k = 0
-
-                while self.i < len(s) and s[self.i].isdigit():
-                    k = k * 10 + int(s[self.i])
-                    self.i += 1
-                print(result)
-                self.i += 1
-                decoded_string = self.decodeString(s)
-                self.i += 1
-                result += decoded_string * k
-        return result
+                res += c
+        return res
