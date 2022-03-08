ca0ad9565bf94adbb7440a3340adddfd6bfe20ed
Henok F <69524541+Henokaa@users.noreply.github.com>
2022-03-08 16:58:38 +0300

    Time: 49 ms (35.95%), Space: 13.8 MB (77.81%) - LeetHub

--- 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.py
+++ 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.py
@@ -6,19 +6,10 @@
 #         self.right = right
 class Solution:
     def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        
         order = []  
         def rec(root):
             if not root:
                 return []
-            
-            if root.left:
-                rec(root.left)
-                
-            if root.right:
-                rec(root.right)
-            
-            order.append(root.val)
-            
-            
-            return order
+            return rec(root.left) + rec(root.right) + [root.val]
         return rec(root)
