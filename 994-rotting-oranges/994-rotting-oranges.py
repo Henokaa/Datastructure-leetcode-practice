c4bbc12af33f8cd8296ec41374a047f6b91b6356
Henok F <69524541+Henokaa@users.noreply.github.com>
2022-03-10 16:10:58 +0300

    Time: 51 ms (91.47%), Space: 14 MB (40.39%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.py
+++ 994-rotting-oranges/994-rotting-oranges.py
@@ -9,18 +9,16 @@ class Solution:
                     fresh += 1
                 if grid[r][c] == 2:
                     q.append([r,c])  #rotten cordination
+        print(q)
         directions = [[0,1], [0, -1], [1, 0], [-1, 0]]
         while q and fresh > 0:
-            for i in range(len(q)):
+            t = len(q)
+            for i in range(t):
                 r, c = q.popleft()
                 for dr, dc in directions:
                     row, col = dr + r, dc + c
-                    print(row)
-                    print(col)
-                    print("round")
                     if (row < 0 or row == len(grid) or
-                        col < 0 or col == len(grid[0]) or
-                        grid[row][col] != 1): 
+                        col< 0 or col == len(grid[0]) or grid[row][col] != 1): 
                         continue
                     grid[row][col] = 2
                     q.append([row, col])
