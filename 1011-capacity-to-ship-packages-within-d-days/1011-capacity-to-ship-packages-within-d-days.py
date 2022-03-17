2184233c94b1f72bd01e8fe211214d2c650b4887
Henok F <69524541+Henokaa@users.noreply.github.com>
2022-03-17 16:38:04 +0300

    Time: 2049 ms (5.03%), Space: 17 MB (75.11%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.py
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.py
@@ -4,19 +4,37 @@ class Solution:
         left, right = max(weights), sum(weights)
         
         def countDays(capacity):
-            days = curr_weight = 0
-            for i in weights:
-                if curr_weight + i > capacity:
-                    curr_weight = 0
-                    days += 1
-                curr_weight += i
+            i = 0
+            myday = 0 
+            
+            while i < len(weights):
+                count = 0
                 
-            return days+1
+                for x in range(i, len(weights)):
+                    if count < mid:
+                        count += weights[x]
+                        i += 1
+                    if count == mid:
+                        print('stuck in the middle!')
+                        break
+                    if count > mid:
+                        count -= weights[x]
+                        i -= 1
+                        break
+                myday += 1
+
+            # days = curr_weight = 0
+            # for i in weights:
+            #     if curr_weight + i > capacity:
+            #         curr_weight = 0
+            #         days += 1
+            #     curr_weight += i
+            return myday
+            # return days+1
         
         while left <= right:
             mid = left + ((right - left)//2)
             d = countDays(mid)
-            # print(d,mid)
             if d <= days:
                 right = mid -1
                 ans = mid
