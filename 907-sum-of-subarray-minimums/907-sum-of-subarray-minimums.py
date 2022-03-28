b3567beac8e19b3720d4e3ef20775c0533a1c4bb
Henok F <69524541+Henokaa@users.noreply.github.com>
2022-03-28 13:46:29 +0300

    Time: 510 ms (83.91%), Space: 18.7 MB (88.63%) - LeetHub

--- 907-sum-of-subarray-minimums/907-sum-of-subarray-minimums.py
+++ 907-sum-of-subarray-minimums/907-sum-of-subarray-minimums.py
@@ -3,8 +3,11 @@ class Solution:
         s = []
         res = 0
         arr = [0] + arr + [0]
-        for i, a in enumerate(arr):
-            while s and a < arr[s[-1]]:
+        # lets take 1 for eg. how many times 1 come be minimum in the left side 2, counting itself. how many times 1 in             # the right side 3. 3 * 2 = 6. can't be addition. because the element before it 3 will affect it {3,1}, [3,1,2]
+        # [3, 1, 2, 4]
+        #
+        for i, val in enumerate(arr):
+            while s and val < arr[s[-1]]:
                 cur_i = s.pop()
                 res += arr[cur_i]*(cur_i - s[-1])*(i - cur_i)
             s.append(i)
