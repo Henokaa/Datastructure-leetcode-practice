4e820e4385835ad3b67623f92c2e5b8ef4a4f803
Henok F <69524541+Henokaa@users.noreply.github.com>
2022-04-13 18:27:14 +0300

    Time: 109 ms (81.25%), Space: 15.5 MB (66.77%) - LeetHub

---
+++ 210-course-schedule-ii/210-course-schedule-ii.py
@@ -0,0 +1,38 @@
+from collections import deque
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        graph={}
+        indegree={}
+        v = numCourses
+        a = prerequisites
+        for i in range(v):
+            graph[i]=[]
+            indegree[i]=0
+        # {0: 0, 1: 0}
+        # print(indegree)
+        for edge in a:
+            child=edge[0]
+            parent=edge[1]
+            indegree[child]+=1
+            graph[parent].append(child)
+        # print(indegree) this is like incoming
+        # {0: 0, 1: 1}
+        # print(graph) childs
+        # {0: [1], 1: []} 
+        sources=deque()
+        for i,j in indegree.items():
+            if(j==0):
+                sources.append(i)
+        # print(sources)
+        # deque([0])
+        ans=[]
+        while sources:
+            c=sources.popleft()
+            ans.append(c)
+            for nbr in graph[c]: # graph[0] - [1] the nodes 
+                indegree[nbr]-=1 # indegree with number in 1 there was one incomming decrease
+                if(indegree[nbr]==0):
+                    sources.append(nbr)
+        if(len(ans)==v):
+            return ans
+        return []
