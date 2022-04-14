e94e2931a7e641031356a0d1a17cbb1084b71648
Henok F <69524541+Henokaa@users.noreply.github.com>
2022-04-14 07:07:23 +0300

    Time: 685 ms (91.90%), Space: 22.1 MB (30.79%) - LeetHub

--- 802-find-eventual-safe-states/802-find-eventual-safe-states.py
+++ 802-find-eventual-safe-states/802-find-eventual-safe-states.py
@@ -2,14 +2,14 @@ class Solution:
     def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
         n = len(graph)
         safe = {}
-        def dfs(i):
+        def dfs(i): # dfs(0)
             if i in safe:
                 return safe[i]
             safe[i] = False
-            for nei in graph[i]:
-                if not dfs(nei):
+            for nei in graph[i]: # all the children of 0
+                if not dfs(nei): # if the children is not safe then it is not safe, if thereiscycle
                     return False
-            safe[i] = True
+            safe[i] = True # if it does it create cycle its true
             return True
             
         res = []
