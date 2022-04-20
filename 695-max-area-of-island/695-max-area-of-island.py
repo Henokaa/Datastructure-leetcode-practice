b1929bc72d07fc3a2e7620c160d1fe0cf14889a7
Henok F <69524541+Henokaa@users.noreply.github.com>
2022-04-20 19:02:02 +0300

    Time: 292 ms (9.68%), Space: 15.5 MB (72.60%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.py
+++ 695-max-area-of-island/695-max-area-of-island.py
@@ -1,51 +1,49 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        parent = defaultdict()
-        rank = defaultdict()
-        direction = [[1,0],[0,1],[-1,0],[0,-1]]
-        in_bound = lambda r, c : 0 <= r < len(grid) and 0 <= c < len(grid[0])
-        for i in range(len(grid)):
-            for j in range(len(grid[i])):
-                if grid[i][j] == 1:
-                    parent[(i,j)] = (i,j)
-                    rank[(i,j)] = 1
-                
-        def findset(x):
-            if x == parent[x]:
-                return x
-            
-            parent[x] = findset(parent[x])
-            return parent[x]
-        
-        def unionset(x,y):
-            x = findset(x)
-            y = findset(y)
-            
-            if x != y:
-                if rank[x] > rank[y]:
-                    parent[y] = x
-                elif rank[x] < rank[y]:
-                    parent[x] = y
-                else:
-                    parent[x] = y
-                    rank[x] += 1
-            
+        parent = {}
+        rank = {}
+        direction = [[1,0], [0, 1], [-1, 0], [0, -1]]
+        inbound = lambda r, c: 0<= r <len(grid) and 0<= c <len(grid[0])
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if grid[i][j] == 1:
-                    for r,c in direction:
-                        ni, nj = i + r, j + c
-                        if in_bound(ni, nj) and grid[ni][nj] == 1 :
-                            unionset((i,j), (ni,nj))
-
-        count = defaultdict()
-        maxcount = 0
-        for k in parent.keys():
-            v = findset(parent[k])
-            if v in count:
-                count[v] += 1
+                    parent[(i,j)] = (i, j)
+                    rank[(i,j)] = 1
+        
+        def findset(x):
+            if x == parent[x]:
+                return x
+            parent[x] = findset(parent[x])
+            return parent[x]
+        
+        def union(x,y):
+            x = findset(x)
+            y = findset(y)
+            
+            if len(x) > len(y):
+                parent[y] = x
+            elif len(x) < len(y):
+                parent[x] = y
             else:
-                count[v] = 1
-            maxcount = max(maxcount, count[v])
-
+                parent[x] = y
+                rank[y] += 1   # why not rank of y
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 1:
+                    for r, c in direction:
+                        nr, nc = i + r, j + c
+                        if inbound(nr, nc) and grid[nr][nc] == 1:
+                            union((i,j), (nr,nc))
+        
+        count = {}
+        maxcount = 0
+        for i in parent.keys():
+            x = findset(i)
+            if x in count:
+                count[x] += 1
+            else:
+                count[x] = 1
+                
+            maxcount = max(maxcount, count[x])
         return maxcount
+                
