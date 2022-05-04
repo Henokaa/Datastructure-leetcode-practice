046e4ba74ebeab4dfa0d407c418560bfa773caae
Henok F <69524541+Henokaa@users.noreply.github.com>
2022-05-04 18:22:55 +0300

    Time: 199 ms (67.42%), Space: 31.7 MB (55.52%) - LeetHub

---
+++ 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.py
@@ -0,0 +1,40 @@
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.isWord = False
+        
+class Trie:
+    
+    def __init__(self):
+        self.root = TrieNode()
+        
+    def insert(self, word: str) -> None:
+        current = self.root
+        
+        for i in word:
+            if i not in current.children:
+                current.children[i] = TrieNode()
+            current = current.children[i]
+            
+        current.isWord = True
+                
+    def search(self, word, isPrefix = False) -> bool:
+        current = self.root
+        
+        for i in word:
+            if i not in current.children:
+                return False
+            current = current.children[i]
+
+        return current.isWord or isPrefix
+        
+
+    def startsWith(self, prefix: str) -> bool:
+        return self.search(prefix, True)
+    #space 0(26*k*w)
+    # time for add delete search o(k) k no char in one word
