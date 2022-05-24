1246a4a4f5d27d20bbb874a0a410e710a6809053
Henok F <69524541+Henokaa@users.noreply.github.com>
2022-05-24 06:38:41 +0300

    Time: 113 ms (68.80%), Space: 17.3 MB (14.36%) - LeetHub

--- 207-course-schedule/207-course-schedule.py
+++ 207-course-schedule/207-course-schedule.py
@@ -1,26 +1,28 @@
-from collections import deque
+from collections import defaultdict
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        preMap = { i: [] for i in range(numCourses)}
-        for crs, pre in prerequisites:
-            preMap[crs].append(pre)
-            
-        visitSet = set()
-        def dfs(crs):
-            if crs in visitSet:
+        preMap = defaultdict(list)
+        c = 0
+        visitedset = set()
+        for i in range(len(prerequisites)):
+            preMap[prerequisites[i][c]].append(prerequisites[i][c+1])
+        
+        def dfs(cru):
+            if cru in visitedset:
                 return False
-            if preMap[crs] == []:
-                return True
-            
-            visitSet.add(crs)
-            for pre in preMap[crs]:
-                if dfs(pre) == False:
-                    return False
-            visitSet.remove(crs)
-            preMap[crs] = []
-            return True
-        for crs in range(numCourses):
-            if not dfs(crs):  
-                # if false return  false
+            visitedset.add(cru)
+            if cru in preMap:
+                for j in preMap[cru]:
+                    if dfs(j) == False:
+                        return False
+                visitedset.remove(cru)
+                preMap[cru] = [] # without this we will have time complexity 
+            else:
+                visitedset.remove(cru)
+                preMap[cru] = []
+                return True        
+        
+        for i in range(numCourses):
+            if dfs(i) == False:
                 return False
         return True
